ab619018d7a515ce3a70dbbcde56cb0f83586b33
Alejandro <78445720+AlejandroMFausti@users.noreply.github.com>
2021-06-15 15:27:54 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize')
 
 //Para conexion -> NombreBasedeDatos, userMysql, passMySQL, {host:host}
 //Definimos una instancia de Sequelize, con los datos de conexion del host local
-const db = new Sequelize('utn2021', 'root', '', {
+const db = new Sequelize('distribuciones', 'root', '', {
     host: 'localhost', //127.0.0.1
     dialect: 'mysql',
     define: {
